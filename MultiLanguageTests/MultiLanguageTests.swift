dece6cb86da16fd96bbe2af044327dc9a4abbc00
戴亮金 <dlj1@meitu.com>
2018-11-07 17:28:43 +0800

    初始化项目工程

---
+++ MultiLanguageTests/MultiLanguageTests.swift
@@ -0,0 +1,36 @@
+//
+//  MultiLanguageTests.swift
+//  MultiLanguageTests
+//
+//  Created by zj-db1140 on 2018/11/7.
+//  Copyright © 2018年 ice. All rights reserved.
+//
+
+import XCTest
+@testable import MultiLanguage
+
+class MultiLanguageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
